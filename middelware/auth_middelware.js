21c58e8e355c22a07245326854583868fb4705a0
Md.Shawon <shawoncse954@gmail.com>
2025-11-19 11:59:23 +0600

    token added

--- middelware/auth_middelware.js
+++ middelware/auth_middelware.js
@@ -4,30 +4,28 @@ const Login = require("../model/login_model/login_model");
 
 const authMiddleware = async (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(" ")[1];   
+        console.log("AUTH HEADER:", req.headers.authorization);
+        console.log("EXTRACTED TOKEN:", req.headers.authorization?.split(" ")[1]);
+
+        const token = req.headers.authorization?.split(" ")[1];
+
         if (!token) {
-            return res.status(401).json({
-                status: 401,
-                message: "Access Denied: No Token Provided"
-            });
-        }   
+            return res.status(401).json({ message: "No token provided" });
+        }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await Login.findById(decoded.userId);
+        const user = await Login.findById(decoded._id);
         if (!user) {
-            return res.status(404).json({
-                status: 404,
-                message: "User not found"
-            });
-        }   
+            return res.status(404).json({ message: "User not found" });
+        }
+
         req.user = user;
         next();
+
     } catch (error) {
-        console.log("Auth Middleware Error:", error);
-        return res.status(500).json({
-            status: 500,    
-            message: "Internal Server Error"
-        });
+        console.error("Auth Middleware Error:", error);
+        return res.status(401).json({ message: "Invalid token" });
     }
 };
 
--- routes/home_title_routes/home_title.js
+++ routes/home_title_routes/home_title.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const HomeTitleController = require('../../controller/home_title/home_title_controller.js');
+const authMiddleware = require('../../middelware/auth_middelware.js');
 const HomeTitleRoutes = express.Router();
 
-HomeTitleRoutes.put('/:id',HomeTitleController.createHomeTitle);
+HomeTitleRoutes.put('/:id',authMiddleware,HomeTitleController.createHomeTitle);
 
 module.exports = HomeTitleRoutes;
