9bb0562f534fadd0b96f2f030c9605e6e6385969
Md.Shawon <shawoncse954@gmail.com>
2025-11-21 19:31:21 +0600

    developer home title

--- model/home_title_model/hometitle.js
+++ model/home_title_model/hometitle.js
@@ -5,6 +5,7 @@ const HomeTitleSchema = new mongoose.Schema({
     title: { type: String, required: true,trim:true   },
     logoText: { type: String, required: true,trim:true  },       
     name: { type: String, required: true,trim:true  },       
+     developer: { type: String, required: true,trim:true  },     
     short_description: { type: String, required: true,trim:true  },       
 
 },{
